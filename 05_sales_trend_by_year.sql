70acb7158a2015723b6fbd746755053740aeffdc
Zhengtao Lyu <160173631+ZhengtaoLyu@users.noreply.github.com>
2025-07-19 10:19:37 +0100

    Delete 05_sales_trend_by_year.sql

--- 05_sales_trend_by_year.sql
+++
@@ -1,8 +0,0 @@
--- ⑤ 销售趋势：按年份（兼容 CSV 数据源）
-SELECT 
-  SUBSTRING("Order Date", 7, 4) AS year,
-  ROUND(SUM("Sales"), 2) AS sales,
-  ROUND(SUM("Profit"), 2) AS profit
-FROM "Superstore"
-GROUP BY year
-ORDER BY year;
