eaced419ba2a51650114f0885c4588d9ea44041f
Zhengtao Lyu <160173631+ZhengtaoLyu@users.noreply.github.com>
2025-07-19 10:16:43 +0100

    Delete 01_overall_sales_profit.sql

--- 01_overall_sales_profit.sql
+++
@@ -1,6 +0,0 @@
--- ① 总销售额、总利润、利润率
-SELECT 
-  ROUND(SUM("Sales"), 2) AS total_sales,
-  ROUND(SUM("Profit"), 2) AS total_profit,
-  ROUND(SUM("Profit") / NULLIF(SUM("Sales"), 0), 4) AS profit_margin
-FROM "Superstore";
